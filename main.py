6a61348fc240131da19377427a9422b1914a0616
Dominik Lewczyński <dominiklew200060@gmail.com>
2023-04-01 22:08:04 +0200

    Add unit conversion function and menu Enable unit selection and input validation Add delay and clear screen after conversion Implement JSON file support for units Refactor code to use functions for unit conversion and menu Improve user interface with clearer messages and prompts

--- main.py
+++ main.py
@@ -1,46 +1,82 @@
-def konwerter_jednostek():
-    jednostki = {
-        "dlugosc": {
-            "metry": 1,
-            "centymetry": 0.01,
-            "milimetry": 0.001,
-            "cale": 0.0254,
-            "stopy": 0.3048,
-            "jardy": 0.9144,
-            "kilometry": 1000,
-            "mile": 1609.34
-        },
-        "waga": {
-            "kilogramy": 1,
-            "dekagramy": 0.1,
-            "gramy": 0.001,
-            "miligramy": 0.000001,
-            "uncje": 0.0283495,
-            "funty": 0.453592,
-            "tony": 1000
-        }
-    }
-
-    kategoria = input("Podaj kategorię jednostek (np. 'dlugosc', 'waga'): ")
-    if kategoria not in jednostki:
-        print("Nieprawidłowa kategoria")
-        return
-
-    jednostka_wejsciowa = input("Podaj jednostkę wejściową: ")
-    if jednostka_wejsciowa not in jednostki[kategoria]:
-        print("Nieprawidłowa jednostka wejściowa")
-        return
-
-    jednostka_wyjsciowa = input("Podaj jednostkę wyjściową: ")
-    if jednostka_wyjsciowa not in jednostki[kategoria]:
-        print("Nieprawidłowa jednostka wyjściowa")
-        return
-
-    wartosc_wejsciowa = float(input("Podaj wartość wejściową: "))
-
-    wartosc_wyjsciowa = wartosc_wejsciowa * jednostki[kategoria][jednostka_wejsciowa] / jednostki[kategoria][jednostka_wyjsciowa]
-    print(f"{wartosc_wejsciowa} {jednostka_wejsciowa} to {wartosc_wyjsciowa} {jednostka_wyjsciowa}")
+import json
+import time
+import os
 
 
-if __name__ == "__main__":
-    konwerter_jednostek()
+def load_units():
+    with open("units.json", "r") as f:
+        units = json.load(f)
+    return units
+
+
+def print_categories(units):
+    print("Available unit categories:")
+    for category in units.keys():
+        print("-", category)
+
+
+def print_units(units, category):
+    print("Available units for {}: ".format(category))
+    for unit in units[category].keys():
+        print("-", unit)
+
+
+def convert_units(units, category, source_unit, target_unit, value):
+    converted_value = value * \
+        units[category][source_unit] / units[category][target_unit]
+    return converted_value
+
+
+if __name__ == '__main__':
+    units = load_units()
+
+    while True:
+        print("|------------------|")
+        print("|  Unit Converter  |")
+        print("| 1. Convert units |")
+        print("| 2. Quit          |")
+        print("|------------------|")
+        print()
+
+        choice = input("Enter your choice (1 or 2): ")
+        if choice == "1":
+            print_categories(units)
+            category = input("Select a unit category: ")
+            while category not in units:
+                print("Invalid category!")
+                category = input("Select a unit category: ")
+            print()
+            print_units(units, category)
+            source_unit = input("Enter the source unit: ")
+            while source_unit not in units[category]:
+                print("Invalid source unit!")
+                source_unit = input("Enter the source unit: ")
+
+            print()
+            target_unit = input("Enter the target unit: ")
+            while target_unit not in units[category]:
+                print("Invalid target unit!")
+                target_unit = input("Enter the target unit: ")
+
+            print()
+            value = float(input("Enter the value to convert: "))
+
+            print()
+            print("Converting {} {} to {}...".format(
+                value, source_unit, target_unit))
+            time.sleep(3)
+            converted_value = convert_units(
+                units, category, source_unit, target_unit, value)
+            print("Result: {:.2f} {} (was {:.2f} {})".format(
+                converted_value, target_unit, value, source_unit))
+
+            input("\nPress Enter to continue...")
+            os.system('cls' if os.name == 'nt' else 'clear')
+
+        elif choice == "2":
+            print("Goodbye!")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            break
+
+        else:
+            print("Invalid chice, please try again.")
